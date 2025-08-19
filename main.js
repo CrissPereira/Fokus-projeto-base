1f4f8ced9b920e290e6ea00a473c446606935531
Cristian Pereira <cristianpereira2v@gmail.com>
2025-08-19 12:14:07 -0300

    Atualizando caminho das imagens

--- main.js
+++ main.js
@@ -14,17 +14,17 @@ const imagem = document.querySelector('.app__image')
 //Foco
 BotaoFoco.addEventListener('click' , () => {
     html.setAttribute('data-contexto' , 'foco')
-    imagem.setAttribute('src' , '/imagens/foco.png')
+    imagem.setAttribute('src' , './imagens/foco.png')
 } )
 
 //Curto
 BotaoCurto.addEventListener('click' , () => {
     html.setAttribute('data-contexto' , 'descanso-curto')
-    imagem.setAttribute('src' , '/imagens/descanso-curto.png')
+    imagem.setAttribute('src' , './imagens/descanso-curto.png')
 } )
 
 //Longo
 BotaoLongo.addEventListener('click' , () => {
     html.setAttribute('data-contexto' , 'descanso-longo')
-    imagem.setAttribute('src' , '/imagens/descanso-longo.png')
+    imagem.setAttribute('src' , './imagens/descanso-longo.png')
 } )
